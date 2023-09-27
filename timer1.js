50a70071a731ed33b1c87ab7c68eff5faa36610c
stephen-fraser <steve.james.fraser@gmail.com>
2023-09-26 18:29:54 -0700

    add timer1

---
+++ timer1.js
@@ -0,0 +1,17 @@
+// take command line inputs process.argv
+// loop through them starting at 2 to avoid user info
+for (let x = 2; x < process.argv.length; x++) {
+  // edge cases to take out non numbers and negative numbers
+  if (!isNaN(process.argv[x])  && process.argv[x] >= 0) {
+  // set Timeout function call
+    setTimeout (() => {
+      // this line causes a beep in terminal
+      process.stdout.write('\x07'); 
+      // input command line timers 
+    }, process.argv[x]); 
+  }
+};
+
+   
+//example input
+// node timer1.js 10 3 5 15 9 
